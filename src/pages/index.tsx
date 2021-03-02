f7405af124162401a4187d58e388bf83ea591684
Gustavo H. Michels <ghmichels@gmail.com>
2021-03-02 19:37:38 -0300

    add top10 categories ex.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,43 @@
-import {  } from '../styles/GlobalStyle';
+import { GetServerSideProps } from 'next';
 import { Title } from '../styles/pages/Home';
 
-export default function Home() {
+interface IProduct {
+  id: string;
+  title: string;
+}
+
+interface HomeProps {
+  recommendedProducts: IProduct[];
+}
+
+export default function Home( { recommendedProducts }: HomeProps ) {
+
   return (
-    <div><Title>Hello World</Title></div>
+    <div>
+      <section>
+        <Title>Produtos</Title>
+
+        <ul>
+          {recommendedProducts.map(recommendedProduct => {
+            return (
+              <li key={recommendedProduct.id}>
+                {recommendedProduct.title}
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+    </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const response = await fetch('http://localhost:3333/recommended');
+  const recommendedProducts = await response.json();
+
+  return {
+    props: {
+      recommendedProducts
+    }
+  }
 }
